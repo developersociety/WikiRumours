f7911846f26c5a30b24a399439723d7d0d2f6907
Daniel Fairhead <daniel@dev.ngo>
2022-03-24 09:11:32 +0000

    Start including comments form using a template for that too...

---
+++ source/templates/rumours/comment_form.php
@@ -0,0 +1,12 @@
+<form name="addCommentForm2" method="post" onSubmit="return false;">
+<div id='addComment' <?php if ($initially_hidden) echo 'class="collapse"'; ?>>
+		<div class='row form-group'>
+			<div class='col-md-10'>
+				<textarea name="new_comment" placeholder="Add your thoughts..." class="form-control"><?php echo @$_POST['new_comment'] ?></textarea>
+			</div>
+			<div class='col-md-2'>
+				<input type="submit" name="submitComment" value="Submit" class="btn btn-info" onClick="validateAddCommentForm(this.form); return false;" />
+			</div>
+		</div>
+	</div>
+</form>
