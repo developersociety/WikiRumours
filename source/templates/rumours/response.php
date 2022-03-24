4c73f3fd806298267c450b42c3eb4d0693315778
Daniel Fairhead <daniel@dev.ngo>
2022-03-24 09:10:45 +0000

    Include first stab at using the form-builder to generate the form.
    I don't know if there's any real benefit, TBH...

--- source/templates/rumours/response.php
+++ source/templates/rumours/response.php
@@ -2,6 +2,9 @@
 $label_class = 'col-lg-3 col-md-3 col-sm-4 col-xs-12 control-label';
 $control_class = 'col-lg-9 col-md-9 col-sm-8 col-xs-12';
 ?>
+<!--
+	<?php echo $form; ?>
+	-->
 <form class="form-horizontal">
 	<div class="form-group">
 		<label class="<?php echo $label_class ?>" for="who">Who:</label>
