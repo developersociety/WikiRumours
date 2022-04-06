49152fa93e8633c0c8c0618408ac25b9a2f2c606
Daniel Fairhead <daniel@dev.ngo>
2022-04-06 17:00:38 +0100

    Fix `make reset` to give full CMS access to dev user.

--- db_setup/add-dev-user.sql
+++ db_setup/add-dev-user.sql
@@ -2,3 +2,12 @@ INSERT INTO `wr_users` (`username`, `password_hash`, `password_score`, `first_na
 VALUES
 	('dev', '$2a$08$rnQ4ryO7wD3HFbBvWnMTjOVyE8QGd3NzI7uxydVnvnNw0LG4AL/lC', 83, 'SOC', 'SOC', '_dev@dev.ngo', '', 0, '', 0, 'AF', 0, '', '', 0, 0, 0, 0, 1, '2022-02-24 04:59:02', 1, 0, '2022-02-24 05:22:30', 1, 0, 1);
 
+
+INSERT INTO `wr_user_permissions`
+	(
+		`user_id`, `name`, `value`, `can_edit_content`, `can_update_settings`, `can_edit_settings`,
+		`can_edit_users`, `can_send_email`, `can_run_housekeeping`
+	)
+VALUES
+	((select user_id from `wr_users` where username='dev'), 'dev', 0, 1, 1, 1, 1, 1, 0);
+
