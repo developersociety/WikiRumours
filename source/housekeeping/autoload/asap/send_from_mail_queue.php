daf5fa5ab382665eb75e7b457fd305f0f0e7693f
Peter Ondruska <ondruska@users.noreply.github.com>
2022-07-14 15:37:59 +0200

    disable sending monitoring emails, stop using email priority

--- source/housekeeping/autoload/asap/send_from_mail_queue.php
+++ source/housekeeping/autoload/asap/send_from_mail_queue.php
@@ -2,11 +2,14 @@
 
 	if ($currentDatabase == 'staging' || $currentDatabase == 'production') {
 
-		$maxMessagesToSend = 5;
+		$maxMessagesToSend = 10;
 
 		// retrieve unsent mail in order of priority, and then in order of earliest queued first
-			$unsentMail = retrieveFromDb('mail_queue', null, null, null, null, null, $tablePrefix . "mail_queue.sent_on = '0000-00-00 00:00:00' AND " . $tablePrefix . "mail_queue.failed_attempts < '" . $tl->settings['Maximum allowable failures per email address'] . "'", null, $tablePrefix . 'mail_queue.priority DESC, ' . $tablePrefix . 'mail_queue.queued_on ASC');
-			$output .= "Found " . count($unsentMail) . " queued messages\n";
+//			$unsentMail = retrieveFromDb('mail_queue', null, null, null, null, null, $tablePrefix . "mail_queue.sent_on = '0000-00-00 00:00:00' AND " . $tablePrefix . "mail_queue.failed_attempts < '" . $tl->settings['Maximum allowable failures per email address'] . "'", null, $tablePrefix . 'mail_queue.priority DESC, ' . $tablePrefix . 'mail_queue.queued_on ASC');
+		// stop sending monitoring emails temporarily (from no-reply to no-reply)
+		// stop ordering by priority for now, descending index is not available with current database engine
+		$unsentMail = retrieveFromDb('mail_queue', null, null, null, null, null, "sent_on = '0000-00-00 00:00:00' AND failed_attempts < " . $tl->settings['Maximum allowable failures per email address'] . " AND NOT (from_email = 'no-reply@msf-listen.org' AND to_email = 'no-reply@msf-listen.org')", null, false, $maxMessagesToSend);
+		$output .= "Found " . count($unsentMail) . " queued messages\n";
 
 			if (count($unsentMail)) {
 
