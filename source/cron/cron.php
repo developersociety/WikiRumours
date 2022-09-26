dd6e3dd74dce1fbcce92541943ae8b7f14723ba8
Daniel Fairhead <daniel@dev.ngo>
2022-09-26 16:34:59 +0100

    Replace web_root w/ wwwroot for cron

--- source/cron/cron.php
+++ source/cron/cron.php
@@ -1,7 +1,7 @@
 #!/usr/local/bin/php -q
 <?php
 
-	include __DIR__ . "/../web_root/initialize.php";
-	if ($tl->settings['Enable cron connections'] && $tl->settings['Interval between cron connections'] > 0) include __DIR__ . "/../web_root/housekeeping.php";
+	include __DIR__ . "/../wwwroot/initialize.php";
+	if ($tl->settings['Enable cron connections'] && $tl->settings['Interval between cron connections'] > 0) include __DIR__ . "/../wwwroot/housekeeping.php";
 		
 ?>
